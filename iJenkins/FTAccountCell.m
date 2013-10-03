a9d40541c816b167b239b951b433db4c7777ebf7
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2013-10-03 19:09:43 +0100

    improving reachability
    Signed-off-by: Ondrej Rafaj <ondrej.rafaj@fuerteint.com>

--- iJenkins/FTAccountCell.m
+++ iJenkins/FTAccountCell.m
@@ -12,7 +12,6 @@
 @interface FTAccountCell ()
 
 @property (nonatomic, strong) UIView *accessIndicator;
-@property (nonatomic, strong) UIActivityIndicatorView   *activityIndicator;
 
 @end
 
@@ -27,8 +26,6 @@
     
     [self.textLabel setXOrigin:36];
     [self.detailTextLabel setXOrigin:36];
-    
-    _activityIndicator.center = _accessIndicator.center;
 }
 
 #pragma mark Creating elements
@@ -46,51 +43,27 @@
     [super createAllElements];
     [self createAccessIndicator];
     
-    _activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
-    _activityIndicator.hidesWhenStopped = YES;
-    [self addSubview:_activityIndicator];
-    
     [self setReachabilityStatus:FTAccountCellReachabilityStatusUnknown];
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-#pragma mark -
 #pragma mark Accessors
 
-- (void)setReachabilityStatus:(FTAccountCellReachabilityStatus)reachabilityStatus
-{
+- (void)setReachabilityStatus:(FTAccountCellReachabilityStatus)reachabilityStatus {
     _reachabilityStatus = reachabilityStatus;
-    
-    BOOL activityIndicatorRunning = NO;
-    BOOL statusIndicatorHidden = NO;
     UIColor *statusIndicatorColor = [UIColor lightGrayColor];
-    
     switch (_reachabilityStatus) {
         case FTAccountCellReachabilityStatusLoading:
-            statusIndicatorHidden = YES;
-            activityIndicatorRunning = YES;
             break;
         case FTAccountCellReachabilityStatusUnreachable:
-            activityIndicatorRunning = NO;
             statusIndicatorColor = [UIColor colorWithHexString:@"FF4000"];
             break;
         case FTAccountCellReachabilityStatusReachable:
-            activityIndicatorRunning = NO;
             statusIndicatorColor = [UIColor colorWithHexString:@"6DD900"];
             break;
         default:
             break;
     }
-    
-    //  Configure view
-    if (activityIndicatorRunning) {
-        [self.activityIndicator startAnimating];
-    } else {
-        [self.activityIndicator stopAnimating];
-    }
-    
-    _accessIndicator.hidden = statusIndicatorHidden;
-    _accessIndicator.backgroundColor = statusIndicatorColor;
+    [_accessIndicator setBackgroundColor:statusIndicatorColor];
 }
 
 
